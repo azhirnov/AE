191f95a0303ac1a52ec19a8c542f65f06929247a
azhirnov <zh1dron@gmail.com>
2019-11-01 18:29:34 +0300

    added task with std::function

---
+++ engine/threading/FunctionTask.h
@@ -0,0 +1,43 @@
+// Copyright (c) 2018-2019,  Zhirnov Andrey. For more information see 'LICENSE'
+
+#pragma once
+
+#include "threading/TaskScheduler.h"
+
+namespace AE::Threading
+{
+
+	//
+	// Function Async Task
+	//
+
+	class FunctionTask final : public IAsyncTask
+	{
+	// types
+	public:
+		using Func_t	= std::function< void () >;
+
+	// variables
+	private:
+		Func_t	_func;
+
+	// methods
+	public:
+		FunctionTask (Func_t &&fn, EThread type = EThread::Worker) :
+			IAsyncTask{ type }, _func{ std::move(fn) }
+		{}
+
+		template <typename Fn>
+		FunctionTask (Fn &&fn, EThread type = EThread::Worker) :
+			IAsyncTask{ type }, _func{ std::forward<Fn>(fn) }
+		{}
+
+	private:
+		void Run () override
+		{
+			return _func();
+		}
+	};
+
+
+}	// AE::Threading
