eebece87046d48202575029a4510075863d0ba11
azhirnov <zh1dron@gmail.com>
2019-11-01 18:36:06 +0300

    added performance test for task scheduler

--- tests/threading/main.cpp
+++ tests/threading/main.cpp
@@ -3,11 +3,13 @@
 #include "stl/Common.h"
 
 extern void UnitTest_TaskDeps ();
+extern void PerfTest_Threading ();
 
 
 int main ()
 {
 	UnitTest_TaskDeps();
+	PerfTest_Threading();
 
 	AE_LOGI( "Tests.Threading finished" );
 	return 0;
