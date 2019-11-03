4b7c62f19672761ae8868968a396d079f5e88d53
azhirnov <zh1dron@gmail.com>
2019-11-03 12:03:15 +0300

    update threading tests

--- tests/threading/UniTest_TaskDeps.cpp
+++ tests/threading/UniTest_TaskDeps.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2018-2019,  Zhirnov Andrey. For more information see 'LICENSE'
 
-#include "threading/WorkerThread.h"
+#include "threading/TaskSystem/WorkerThread.h"
 #include "UnitTest_Common.h"
 
 
@@ -33,22 +33,22 @@ namespace
 				value = (value << 4) | 2;
 			}
 		};
-
-		TaskScheduler	scheduler;
+		
+		LocalTaskScheduler	scheduler {1};
 
 		uint			value	= 1;
 		const uint		required = (((1 << 4) | 1) << 4) | 2;
 
-		AsyncTask		task1	= scheduler.Run<Task1>( Array<AsyncTask>{}, std::ref(value) );
-		AsyncTask		task2	= scheduler.Run<Task2>( Array<AsyncTask>{task1}, std::ref(value) );
+		AsyncTask		task1	= scheduler->Run<Task1>( Array<AsyncTask>{}, std::ref(value) );
+		AsyncTask		task2	= scheduler->Run<Task2>( Array<AsyncTask>{task1}, std::ref(value) );
 		TEST( task1 and task2 );
 
-		scheduler.AddThread( MakeShared<WorkerThread>() );
+		scheduler->AddThread( MakeShared<WorkerThread>() );
 		
-		TEST( scheduler.Wait({ task1, task2 }));
+		TEST( scheduler->Wait({ task1, task2 }));
 		TEST( value == required );
-		TEST( task1->Status() == IAsyncTask::EStatus::Complete );
-		TEST( task2->Status() == IAsyncTask::EStatus::Complete );
+		TEST( task1->Status() == IAsyncTask::EStatus::Completed );
+		TEST( task2->Status() == IAsyncTask::EStatus::Completed );
 	}
 
 	
@@ -66,7 +66,7 @@ namespace
 				value = (value << 4) | 1;
 			}
 
-			void Cancel () override
+			void OnCancel () override
 			{
 				value = (value << 4) | 2;
 			}
@@ -84,27 +84,27 @@ namespace
 				value = (value << 4) | 2;
 			}
 
-			void Cancel () override
+			void OnCancel () override
 			{
 				value = (value << 4) | 1;
 			}
 		};
-
-		TaskScheduler	scheduler;
+		
+		LocalTaskScheduler	scheduler {1};
 
 		uint			value	= 1;
 		const uint		required = (((1 << 4) | 2) << 4) | 1;
 
-		AsyncTask		task1	= scheduler.Run<Task1>( Array<AsyncTask>{}, std::ref(value) );
+		AsyncTask		task1	= scheduler->Run<Task1>( Array<AsyncTask>{}, std::ref(value) );
 		TEST( task1 );
-		scheduler.Cancel( task1 );
+		scheduler->Cancel( task1 );
 
-		AsyncTask		task2	= scheduler.Run<Task2>( Array<AsyncTask>{task1}, std::ref(value) );
+		AsyncTask		task2	= scheduler->Run<Task2>( Array<AsyncTask>{task1}, std::ref(value) );
 		TEST( task2 );
 
-		scheduler.AddThread( MakeShared<WorkerThread>() );
+		scheduler->AddThread( MakeShared<WorkerThread>() );
 		
-		TEST( scheduler.Wait({ task1, task2 }));
+		TEST( scheduler->Wait({ task1, task2 }));
 		TEST( value == required );
 		TEST( task1->Status() == IAsyncTask::EStatus::Canceled );
 		TEST( task2->Status() == IAsyncTask::EStatus::Canceled );
--- tests/threading/UnitTest_Common.h
+++ tests/threading/UnitTest_Common.h
@@ -2,8 +2,27 @@
 
 #pragma once
 
-#include "threading/TaskScheduler.h"
+#include "threading/TaskSystem/TaskScheduler.h"
 
 using namespace AE::Threading;
 
 #define TEST	CHECK_FATAL
+
+
+struct LocalTaskScheduler
+{
+	LocalTaskScheduler (size_t maxWorkerThreads)
+	{
+		Scheduler().Setup( maxWorkerThreads );
+	}
+
+	~LocalTaskScheduler ()
+	{
+		Scheduler().Release();
+	}
+
+	TaskScheduler* operator -> ()
+	{
+		return &Scheduler();
+	}
+};
